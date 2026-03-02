1bdd3c6c935a81f6a2214f63f3769fedf1202b63
Ayush Patel <ayush005rkt@gmail.com>
2026-03-02 23:03:36 +0530

    email

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -5,22 +5,22 @@ module.exports = async (email, subject, text) => {
 	try {
 		const transporter = nodemailer.createTransport({
 			service: "gmail",
-			port:465,
+			port: 465,
 			secure: true,
-            logger:true,
-            debug:true,
-            secureConnection:false,
+			logger: true,
+			debug: true,
+			secureConnection: false,
 			auth: {
-				user: process.env.USER,
-				pass: process.env.PASS,
+				user: process.env.EMAIL_USER,
+				pass: process.env.EMAIL_PASS,
 			},
-            tls:{
-                rejectUnauthorized:true
-            }
+			tls: {
+				rejectUnauthorized: true
+			}
 		});
 
 		await transporter.sendMail({
-			from: process.env.USER,
+			from: process.env.EMAIL_USER,
 			to: email,
 			subject: subject,
 			text: text,
