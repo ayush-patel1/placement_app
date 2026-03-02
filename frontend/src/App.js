50346cec1fb9283b90b7dfca82a84e60199cc5c7
Ayush Patel <ayush005rkt@gmail.com>
2026-03-02 21:09:01 +0530

    final frontend commit

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./scenes/Home";
+import Dsa from "./scenes/Dsa";
+import MachineLearn from "./scenes/MachineLearn";
+import Csfundamental from "./scenes/Csfundamental";
+import Webd from "./scenes/Webd";
+import QuesList from "./scenes/QuesList";
+import ResumeMaker from "./scenes/ResumeMaker";
+import Login from "./scenes/Login";
+import { AuthProvider } from "./AuthContext";
+import EmailVerify from "./scenes/EmailVerify"
+
+function App() {
+  return (
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/resumemaker' element={<ResumeMaker />} />
+            <Route path='/dsa' element={<Dsa />} />
+            <Route path='/ml' element={<MachineLearn />} />
+            <Route path='/csfundamental' element={<Csfundamental />} />
+            <Route path='/webd' element={<Webd />} />
+            <Route path="/dsa/question/:id" element={<QuesList />} />
+            <Route path="/:id/verify/:token" element={<EmailVerify />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </>
+  );
+}
+
+export default App;
